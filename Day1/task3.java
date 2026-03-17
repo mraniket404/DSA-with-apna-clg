3f7ecad6062463aced64a8911e80dbb56777d1c9
mraniket404 <aniketgosavi471@gmail.com>
2026-03-17 10:22:02 +0530

    task3

---
+++ Day1/task3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;  
+public class task3 {
+    public static int max(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        System.out.println("Maximum of 3 numbers is: " + max(num1, num2, num3));
+
+    }
+}
