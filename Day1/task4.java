1d2b9f039dc5906d611bb32c8ca3faefba485f03
mraniket404 <aniketgosavi471@gmail.com>
2026-03-17 11:06:08 +0530

    task4

--- Day1/task4.java
+++ Day1/task4.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
+
 public class task4 {
-    public static boolean prime(int n) {
-        if (n <= 1) {
-            return false;
+    public static String prime(int num) {
+        if (num <= 1) {
+            return "Not a prime number";
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return "Not a prime number";
             }
         }
-        return true;
+        return "Prime number";
         
-    }
+    } 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = sc.nextInt();
-        System.out.println( "Is " + num + " a prime number == " + prime(num));
+        System.out.println(prime(num));
         }
     }
 
