f615ef012d53d02b31e5bb6bbbb4b15a480802d3
mraniket404 <aniketgosavi471@gmail.com>
2026-04-04 12:35:02 +0530

    even odd

---
+++ ConditionalStatement/Question/q2.java
@@ -0,0 +1,12 @@
+// odd or even 
+public class q2 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if(number % 2 == 0){
+            System.out.println("the number is even");
+        }else{
+            System.out.println("the number is odd");
+        }
+    }
+}
