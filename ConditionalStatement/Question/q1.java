d6310708b304ecba80c5fda495f7ef6d86d326e1
mraniket404 <aniketgosavi471@gmail.com>
2026-04-04 12:03:42 +0530

    largest number of 2

---
+++ ConditionalStatement/Question/q1.java
@@ -0,0 +1,15 @@
+// print largest number among 2 numbers
+public class q1 {
+    public static void main(String[] args) {
+      int num1 = 10 ;
+      int num2 = -10 ;
+    
+      if(num1<num2){
+        System.out.println("the number 2 is big");
+      }else if (num1>num2){
+        System.out.println("the number 1 is big");
+      }else{
+        System.out.println("The number are equal");
+      }
+    }
+}
