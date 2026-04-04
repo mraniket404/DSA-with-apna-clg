16d4df6e19693ea1edd2bfa99c46d5934709fcc5
mraniket404 <aniketgosavi471@gmail.com>
2026-04-04 11:49:20 +0530

    if-else statement

---
+++ ConditionalStatement/if-else-Statement/basic1.java
@@ -0,0 +1,13 @@
+
+public class basic1 {
+    public static void main(String[] args) {
+        int age = 20 ;
+
+        if (age <=18){
+            System.out.println("You are a minor");
+        }
+        else {
+            System.out.println("You are an adult");
+        }
+    }
+}
