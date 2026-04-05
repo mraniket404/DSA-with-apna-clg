c85a85c9a38368a7d0c289d65635ed28f0de0ed4
mraniket404 <aniketgosavi471@gmail.com>
2026-04-05 12:11:36 +0530

    for loop reverse of a number

---
+++ loops/forLoops/Q3.java
@@ -0,0 +1,13 @@
+//reverse of a number
+
+public class Q3 {
+    public static void main(String[] args) {
+        
+        for (int n = 1089980;n>0;){
+            int lastdigit = n%10;
+            System.out.print(lastdigit+"");
+            n = n/10;
+        }
+        // System.out.println();
+    }
+}
---
+++ loops/forLoops/q2.java
@@ -0,0 +1,8 @@
+// print square pattern 
+public class q2 {
+    public static void main(String[] args) {
+        for (int line = 1;line <=4;line++){
+            System.out.println("* * * *");
+        }
+    }
+}
