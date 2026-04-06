691966d06580432fa5241890de2f97f40049873c
mraniket404 <aniketgosavi471@gmail.com>
2026-04-07 00:54:00 +0530

    character printing

---
+++ patterns/Q4.java
@@ -0,0 +1,19 @@
+// character printing 
+// A
+// BC
+// DEF
+// GHIJ
+public class Q4 {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        for (int line = 1;line<=n;line++){
+            for (int chars =1;chars<=line;chars++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+}
