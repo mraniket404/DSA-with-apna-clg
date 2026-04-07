0f186a01d5770cba33ba86f7f8ca2455075e1267
mraniket404 <aniketgosavi471@gmail.com>
2026-04-07 21:47:31 +0530

    swiping with function

--- FunctionMethods/Q2.java
+++ FunctionMethods/Q2.java
@@ -1,11 +1,33 @@
 public class Q2 {
-    public static void main(String[] args) {
-        int a  = 2;
-        int b = 5;
-         
+    public static int[] swip(int a, int b) {
         int temp = a;
+        a = b;
+        b = temp;
+        return new int[]{a, b};  
+    }
+    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
 
-        System.out.println("the value of a is : "+ (a=b));        
-        System.out.print("the value of b is : "+(b=temp));        
+        // frist 
+         // int temp = a;
+        // a=b;
+        // b=temp;
+        // System.out.println(a);
+        // System.out.println(b);
+        
+        //second two type me likh sakte he 
+        //1st
+        System.out.println(swip(a,b)[0]);
+        System.out.println(swip(a,b)[1]);
+        
+        //2nd
+        // int[] swapped = swip(a, b);
+        
+        // System.out.println("a = " + swapped[0]);
+        // System.out.println("b = " + swapped[1]);
+
+       
     }
 }
---
+++ FunctionMethods/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+ // System.out.println(swip(a,b)[0]);
+        // System.out.println(swip(a,b)[1]);
