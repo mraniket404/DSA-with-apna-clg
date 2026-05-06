f263a76581776b94b0e6c78fffd4d4d6959cd11a
mraniket404 <aniketgosavi471@gmail.com>
2026-05-06 10:14:28 +0530

    floyds_triangle

---
+++ advanced_pattern/floyds_triangle.java
@@ -0,0 +1,21 @@
+     class  floyds_triangle {
+        public static void floyds_triangle(int n){
+            int counter = 1;
+            // outer_loop:
+            for(int i=1;i<=n;i++){
+                // inner loops - how many times counter be printed
+                for(int j=1;j<=i;j++){
+                    System.out.print(counter + " ");
+                    counter++;
+                }
+                System.out.println();
+                
+            }
+            
+        }
+        public static void main(String[] args) {
+        int n=7;
+        floyds_triangle(n);
+
+    }
+}
