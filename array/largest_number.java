dd5ea593c7ea3a8a11bd5b1e4775eada9fe3a480
mraniket404 <aniketgosavi471@gmail.com>
2026-05-13 09:56:53 +0530

    larges and smallest number in the array ]

--- array/largest_number.java
+++ array/largest_number.java
@@ -2,12 +2,17 @@ public class largest_number {
 
     public static int largest_number(int numbers[]) {
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > largest) {
                 largest = numbers[i];
+            } if (numbers[i] < smallest) {
+                smallest = numbers[i];
             }
         }
+        System.out.println("Smallest number is : " + smallest);
         return largest;
+        
     }
 
     public static void main(String[] args) {
