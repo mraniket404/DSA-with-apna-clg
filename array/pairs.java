15174824f525a7a96d1c12f476af17794fd56692
mraniket404 <aniketgosavi471@gmail.com>
2026-05-14 16:53:48 +0530

    total pair abhi calculage kiya

--- array/pairs.java
+++ array/pairs.java
@@ -1,12 +1,15 @@
 public class pairs {
     public static void pairs_array(int numbers []) {
+        int tp = 0;
         for (int i = 0; i < numbers.length; i++) {
             int current = numbers[i];
             for (int j = i + 1; j < numbers.length; j++) {
                 System.out.println("(" + current + ", " + numbers[j] + ")");
+                tp++;
             }  
             System.out.println("------------------"); 
         }
+        System.out.println("Total pairs: " + tp);
     }
     public static void main(String[] args) {
         int numbers []= {1,2,3,4,5,6,7,8,9};
