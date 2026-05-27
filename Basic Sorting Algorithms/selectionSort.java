d0ebfe4d36516c2aea16d7dfa3c9895d362ed8e9
mraniket404 <aniketgosavi471@gmail.com>
2026-05-27 11:32:38 +0530

    Selection sorting

---
+++ Basic Sorting Algorithms/selectionSort.java
@@ -0,0 +1,26 @@
+
+public class selectionSort {
+    public static void selection(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i<n-1;i++){
+            int min = i;
+            for (int j = i+1; j<n;j++){
+                if (arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        selection(arr);
+        
+    }
+}
