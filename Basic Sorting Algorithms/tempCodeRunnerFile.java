0eba7396541f0abc2fb2003803dc451baec4912b
mraniket404 <aniketgosavi471@gmail.com>
2026-06-16 01:53:40 +0530

    insertionSort

---
+++ Basic Sorting Algorithms/tempCodeRunnerFile.java
@@ -0,0 +1,29 @@
+public class insertionSort {
+
+    public static void insertion(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are greater than key,
+               to one position ahead of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            // Place the key at its correct position
+            arr[j + 1] = key;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    
+    }
+    public static void main(String[] args) {
+      int arr[] = {5,4,1,3,2};
+        
+        insertion(arr); 
+        
+    }
+}
